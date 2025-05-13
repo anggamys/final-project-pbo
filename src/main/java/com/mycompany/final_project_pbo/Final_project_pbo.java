d9b8fb700c9fe24690a28492efcbd1f324ad4bf2
anggamys <anggayunus139@gmail.com>
2025-05-13 19:14:54 +0700

    first commit

---
+++ src/main/java/com/mycompany/final_project_pbo/Final_project_pbo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.final_project_pbo;
+
+/**
+ *
+ * @author c0delb08
+ */
+public class Final_project_pbo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
