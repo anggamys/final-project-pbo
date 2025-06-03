145487c211317da141096ff939dfa988cd303b37
dyahinkud <dyahinkud@gmail.com>
2025-06-03 18:49:11 +0700

    fix: correct package declarations in utility classes for consistency

--- src/main/java/com/mycompany/final_project_pbo/utils/CrudRepository.java
+++ src/main/java/com/mycompany/final_project_pbo/utils/CrudRepository.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.final_project_pbo;
+package com.mycompany.final_project_pbo.utils;
 
 import java.util.ArrayList;
 
@@ -12,7 +12,13 @@ import java.util.ArrayList;
  * @param <T>
  */
 public interface CrudRepository<T> {
-    Response<T> findById(int id);
-    Response<Boolean> deleteById(int id);
+    Response<T> save(T entity);
+
+    Response<T> update(T entity);
+
+    Response<T> findById(Integer id);
+
+    Response<Boolean> deleteById(Integer id);
+
     Response<ArrayList<T>> findAll();
 }
--- src/main/java/com/mycompany/final_project_pbo/utils/DatabaseUtil.java
+++ src/main/java/com/mycompany/final_project_pbo/utils/DatabaseUtil.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.final_project_pbo.Utils;
+package com.mycompany.final_project_pbo.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
